dd1bc18bf0315ff86409bfebd7ce8729100f055c
jenjenPSkabel <jjp120033@gmail.com>
2025-09-28 21:37:47 -0700

    Update user.py

--- user.py
+++ user.py
@@ -1,4 +1,9 @@
-import socket, json, argparse, time
+import socket, json, argparse
+
+def send(sock, mgr, msg):
+    sock.sendto(json.dumps(msg).encode(), mgr)
+    data, _ = sock.recvfrom(12000)
+    return json.loads(data.decode("utf-8"))
 
 def main():
     ap = argparse.ArgumentParser()
@@ -11,23 +16,30 @@ def main():
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(("0.0.0.0", args.my_m_port))
-    msg = {
+    mgr = (args.manager_ip, args.manager_port)
+
+    r = send(sock, mgr, {
         "cmd": "register-user",
         "args": {"user_name": args.user_name, "ip": "127.0.0.1",
                  "m_port": args.my_m_port, "c_port": args.my_c_port}
-    }
-    sock.sendto(json.dumps(msg).encode(), (args.manager_ip, args.manager_port))
+    })
+    print("register-user ->", r)
 
-    data, _ = sock.recvfrom(12000)
-    print(json.loads(data.decode("utf-8")))
-
-    print("User registered. Ctrl+C to exit.")
-    try:
-        while True:
-            time.sleep(60)
-    except KeyboardInterrupt:
-        pass
+    print("Type commands: ls | quit")
+    while True:
+        try:
+            line = input("> ").strip().lower()
+        except EOFError:
+            break
+        if line in ("quit", "exit"):
+            break
+        elif line == "ls":
+            r = send(sock, mgr, {"cmd": "ls", "args": {}})
+            print(json.dumps(r, indent=2))
+        elif line == "":
+            continue
+        else:
+            print("unknown command")
 
 if __name__ == "__main__":
     main()
-
